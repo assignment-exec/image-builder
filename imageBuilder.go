463703d224cc8fa2f099999dac4f886d2eca8b5d
balandi1 <balandi@binghamton.edu>
2020-04-12 14:40:36 -0400

    Updated imageBuilder.go

--- imageBuilder.go
+++ imageBuilder.go
@@ -1,35 +1,11 @@
 package main
 
 import (
-<<<<<<< HEAD
-	"assignment-exec/image-builder/dockerConfig"
-	"fmt"
-=======
 	"assignment-exec/image-builder/configurations"
->>>>>>> 500abb0ba41560bd9316ac21f9083176deb1b722
 	"log"
 )
 
 func main() {
-<<<<<<< HEAD
-	fmt.Println("Creating Dockerfile...")
-<<<<<<< HEAD
-	config,err := dockerConfig.GetConfig("config.yaml")
-=======
-
-	err := dockerConfig.WriteDockerfile()
->>>>>>> 5917893... Minor changes and fixes
-	if err != nil {
-		log.Fatalf("error while writing dockerfile %v", err)
-	}
-<<<<<<< HEAD
-
-	fmt.Println(config)
-}
-=======
-}
->>>>>>> 5917893... Minor changes and fixes
-=======
 	log.Println("Creating Dockerfile...")
 
 	// Unmarshal the yaml configuration file and generate a dockerfile.
@@ -38,4 +14,3 @@ func main() {
 		log.Fatalf("error while writing dockerfile %v", err)
 	}
 }
->>>>>>> 500abb0ba41560bd9316ac21f9083176deb1b722
