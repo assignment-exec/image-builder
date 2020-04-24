77e5a9342b266150f862caf1ed466f8a105b7abb
balandi1 <balandi@binghamton.edu>
2020-04-24 15:07:21 -0400

    Initial code for pushing image to docker hub

--- builder/builder.go
+++ builder/builder.go
@@ -5,16 +5,46 @@ import (
 	"assignment-exec/image-builder/constants"
 	"bytes"
 	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v3"
 	"io"
 	"io/ioutil"
 	"log"
 	"os"
 )
 
+type dockerAuthData struct {
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	Repository string `yaml:"repository"`
+	Version string `yaml:"version"`
+	DockerfilePath string `yaml:"dockerfilePath"`
+}
+
+// Get the docker authentication details.
+func GetAuthData(filename string) (*dockerAuthData, error) {
+
+	yamlFile, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read log config file")
+	}
+
+	c := &dockerAuthData{}
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Error in unmarshalling yaml: %v", err)
+	}
+
+	return c, nil
+}
+
 // Builds Docker Image for Code Runner.
-func BuildImage() error {
+func BuildImage(authData dockerAuthData) error {
 
 	buildContext := context.Background()
 	dockerClient, err := client.NewEnvClient()
@@ -36,7 +66,7 @@ func BuildImage() error {
 	dockerFilepath := constants.DockerFilepath
 	dockerFileReader, err := os.Open(dockerFilepath)
 	if err != nil {
-		log.Fatalf(" unable to open dockerfile: %v", err)
+		log.Fatalf(" unable to open dockerfile: %v",err)
 	}
 	readDockerFile, err := ioutil.ReadAll(dockerFileReader)
 	if err != nil {
@@ -53,7 +83,7 @@ func BuildImage() error {
 	}
 	_, err = writer.Write(readDockerFile)
 	if err != nil {
-		log.Fatalf("unable to write dockerfile into tar body: %v", err)
+		log.Fatalf("unable to write tar body: %v", err)
 	}
 
 	// Use the tar of the Dockerfile while building image.
@@ -62,8 +92,9 @@ func BuildImage() error {
 	response, err := dockerClient.ImageBuild(
 		buildContext,
 		dockerFileTarReader,
-		types.ImageBuildOptions{Dockerfile: dockerFilepath})
-
+		types.ImageBuildOptions{
+			Dockerfile: dockerFilepath,
+			Tags: []string {fmt.Sprintf("%s/%s:%s",authData.Username, authData.Repository,authData.Version)}})
 	if err != nil {
 		log.Fatalf("unable to build docker image: %v", err)
 	}
@@ -79,5 +110,52 @@ func BuildImage() error {
 		log.Fatalf("unable to read image build response: %v", err)
 	}
 
-	return err
+	return  err
 }
+
+func PushImageToHub(authData dockerAuthData) error {
+
+	// TODO: setup ssh keys for logging into docker hub
+
+	// TODO: Handle the errors appropriately ans return
+
+	buildContext := context.Background()
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return err
+	}
+
+	authConfig := types.AuthConfig{
+		Username: authData.Username,
+		Password: authData.Password,
+	}
+	encodedJSON, err := json.Marshal(authConfig)
+	if err != nil {
+		return fmt.Errorf("error when encoding authConfig. err: %v", err)
+	}
+
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+
+	imageString := fmt.Sprintf("docker.io/%s/%s:%s",authData.Username, authData.Repository,authData.Version)
+	resp, err := cli.ImagePush(buildContext, imageString, types.ImagePushOptions{
+		RegistryAuth: authStr,
+	})
+	if err != nil {
+		panic(err)
+	}
+	_, err = io.Copy(os.Stdout, resp)
+	if err != nil {
+		log.Fatalf("unable to read image push response: %v", err)
+	}
+	defer func() {
+		err = resp.Close()
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+	}()
+
+	return err
+
+}
+
--- imageBuilder.go
+++ imageBuilder.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"assignment-exec/image-builder/builder"
-	"assignment-exec/image-builder/configurations"
 	"log"
 )
 
@@ -10,12 +9,22 @@ func main() {
 	log.Println("Creating Dockerfile...")
 
 	// Unmarshal the yaml configuration file and generate a dockerfile.
-	err := configurations.WriteDockerfile()
+	/*err := configurations.WriteDockerfile()
 	if err != nil {
 		log.Fatalf("error while writing dockerfile: %v", err)
+	}*/
+
+	authData, err := builder.GetAuthData("docker-auth.yaml")
+	if err != nil {
+		log.Fatalf("error while reading authetication details: %v", err)
 	}
 
-	err = builder.BuildImage()
+	err = builder.BuildImage(*authData)
+	if err != nil {
+		log.Fatalf("error while building image for code runner: %v", err)
+	}
+
+	err = builder.PushImageToHub(*authData)
 	if err != nil {
 		log.Fatalf("error while building image for code runner: %v", err)
 	}
