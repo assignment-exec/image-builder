a95234628f29d8edf5fca7577aa8660324c5dbf5
balandi1 <balandi@binghamton.edu>
2020-05-07 19:42:57 -0400

    Modified installation scripts
    - Added -y option for avoiding abort while installation

--- scripts/python_3.7.sh
+++ scripts/python_3.7.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 # Installation commands for python 3.7
-apt-get update
-apt install software-properties-common
-add-apt-repository ppa:deadsnakes/ppa
-apt install python3.7
+apt-get update -y
+apt-get install python3.7 -y
+apt-get update -y
+apt-get install python3-pip -y
 alias python=python3
-apt-get update
-apt install python3-pip
