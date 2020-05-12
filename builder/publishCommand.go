2f82b4a0e5314f8d0ec4fd1fd9b4ad39b7d660b1
balandi1 <balandi1@binghamton.edu>
2020-05-12 17:46:35 -0400

    Added code to print out final docker run command

--- builder/publishCommand.go
+++ builder/publishCommand.go
@@ -1,6 +1,8 @@
 package builder
 
 import (
+	"assignment-exec/image-builder/constants"
+	"fmt"
 	"github.com/pkg/errors"
 )
 
@@ -9,7 +11,15 @@ type publishCommand struct {
 }
 
 func (cmd *publishCommand) execute() error {
-	return cmd.assgnEnv.publish()
+
+	if err := cmd.assgnEnv.publish(); err != nil {
+		return err
+	}
+	dockerRunCmd := fmt.Sprintf("%s %s %s", constants.DockerRunCommand,
+		cmd.assgnEnv.ImgBuildConfig.imageTag, constants.PortCmdArg)
+	fmt.Printf("\nFollowing is the command for starting %s\n\n", cmd.assgnEnv.ImgBuildConfig.imageTag)
+	fmt.Println(dockerRunCmd)
+	return nil
 }
 
 func (cmd *publishCommand) undo() error {
--- constants/constants.go
+++ constants/constants.go
@@ -5,3 +5,5 @@ const DockerIO = "docker.io"
 const BuildContextTar = "buildContext.tar"
 
 const CodeRunnerDir = "code-runner"
+const DockerRunCommand = "docker run --publish 52453:52453"
+const PortCmdArg = "-port 52453"
